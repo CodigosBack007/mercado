5b338eb8895135e12b904b5e7c194858ff18c8d1
JoaoGabriel0800 <joaogabrielfaria080b@gmail.com>
2025-11-19 14:24:57 -0300

    ajeitando os controller

--- src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
@@ -1,10 +1,15 @@
 package br.com.senai.backend.sistema_mercado.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.senai.backend.sistema_mercado.models.Funcionario;
 import br.com.senai.backend.sistema_mercado.services.FuncionarioService;
+
 
 @RestController
 @RequestMapping("/funcionario")
@@ -12,5 +17,16 @@ public class FuncionarioController {
     @Autowired
     private FuncionarioService funService;
 
+    @PostMapping("/cadastrar")
+    public Funcionario cadastrar(@RequestBody Funcionario funcionario) {
+        return funService.cadastrar(funcionario);
+    }
+
+    @GetMapping("/recuperar")
+    public Funcionario recuperarPorId(@RequestBody Integer id) {
+        return funService.recuperarPorId(id);
     
+    
+    
+}
 }
--- src/main/java/br/com/senai/backend/sistema_mercado/controllers/MercadoriaController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controllers/MercadoriaController.java
@@ -1,10 +1,15 @@
 package br.com.senai.backend.sistema_mercado.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.senai.backend.sistema_mercado.models.Mercadoria;
 import br.com.senai.backend.sistema_mercado.services.MercadoriaService;
+
 
 @RestController
 @RequestMapping("/Mercadoria")
@@ -12,5 +17,16 @@ public class MercadoriaController {
     @Autowired
     private MercadoriaService merService;
 
+    @PostMapping("/cadastrar")
+    public Mercadoria cadastrar(@RequestBody Mercadoria mercadoria) {
+        return merService.cadastrar(mercadoria);
+    }
+
+    @GetMapping("/recuperar")
+    public Mercadoria recuperarPorId(@RequestBody Integer id) {
+        return merService.recuperarPorId(id);
     
+    
+    
+}
 }
